6a16e8f91ce2f3dc877ec3a146858130f081b876
AliYousifThe2nd <alooy3d@gmail.com>
2022-07-03 14:49:42 +0300

    Clean up appspec hooks

---
+++ deploy-scripts/before-install.ps1
@@ -0,0 +1,10 @@
+function DeleteIfExistsAndCreateEmptyFolder($dir )
+{
+    if ( Test-Path $dir ) {    
+           Get-ChildItem -Path  $dir -Force -Recurse | Remove-Item -force �recurse
+           Remove-Item $dir -Force
+    }
+    New-Item -ItemType Directory -Force -Path $dir
+}
+DeleteIfExistsAndCreateEmptyFolder("C:\temp\TestAPIProject\out" )
+DeleteIfExistsAndCreateEmptyFolder("C:\inetpub\wwwroot\TestAPI\")
